17601522775e7106a9067720e379cf3d73a0209f
Kurt Gardiner <k.a.gardiner@gmail.com>
2014-11-06 19:08:18 +1100

    Fix some features tests

--- features/step_definitions/vmesh.create_steps.rb
+++ features/step_definitions/vmesh.create_steps.rb
@@ -5,5 +5,5 @@ end
 =end
 
 When /^I create with "([^"]*)" template and name "([^"]*)"$/ do |type, name|
-  step %(I run `vmesh create #{type} #{name}`)
+  step %(I run `vmesh -u user -p fakepass create #{name} #{type}`)
 end
--- features/step_definitions/vmesh_steps.rb
+++ features/step_definitions/vmesh_steps.rb
@@ -1,5 +1,5 @@
 When /^I (.*) with no arguments$/ do |command|
-  step %(I run `vmesh #{command}`)
+  step %(I run `vmesh -u user -p fake #{command}`)
 end
 
 When /^I get help for "([^"]*)"$/ do |app_name|
